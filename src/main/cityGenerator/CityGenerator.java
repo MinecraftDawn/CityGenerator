1eaf98dce67e921997bb09428478427b84c8a926
MinecraftDawn <eric4902077@gmail.com>
2019-03-06 23:03:58 +0800

    Create a hills

--- src/main/cityGenerator/CityGenerator.java
+++ src/main/cityGenerator/CityGenerator.java
@@ -1,12 +1,32 @@
 package main.cityGenerator;
 
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CityGenerator extends JavaPlugin {
+public class CityGenerator extends JavaPlugin{
 
     @Override
     public void onEnable(){
 
     }
 
+    @Override
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        return new testGenerator();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if(args.length > 0) {
+            WorldCreator worldCreator = new WorldCreator(args[0]);
+            worldCreator.generator(new testGenerator());
+            worldCreator.createWorld();
+        }
+        return true;
+    }
 }
---
+++ src/main/cityGenerator/testGenerator.java
@@ -0,0 +1,34 @@
+package main.cityGenerator;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.util.noise.SimplexOctaveGenerator;
+
+import java.util.Random;
+
+public class testGenerator extends ChunkGenerator {
+
+    int currentHeight = 80;
+
+    @Override
+    public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, ChunkGenerator.BiomeGrid biome) {
+        ChunkData chunk = createChunkData(world);
+
+        SimplexOctaveGenerator generator = new SimplexOctaveGenerator(new Random(world.getSeed()), 8);
+        generator.setScale(0.005D);
+
+        for (int X = 0; X < 16; X++)
+            for (int Z = 0; Z < 16; Z++) {
+                currentHeight = (int) (generator.noise(chunkX * 16 + X, chunkZ * 16 + Z, 0.5D, 0.5D) * 15D + 50D);
+                chunk.setBlock(X, currentHeight, Z, Material.GRASS);
+                chunk.setBlock(X, currentHeight - 1, Z, Material.DIRT);
+                for (int i = currentHeight - 2; i > 0; i--)
+                    chunk.setBlock(X, i, Z, Material.STONE);
+                chunk.setBlock(X, 0, Z, Material.BEDROCK);
+            }
+        return chunk;
+    }
+
+
+}
