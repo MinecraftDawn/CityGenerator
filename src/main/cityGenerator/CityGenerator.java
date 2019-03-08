5fac804e58359dc1da249a0c309cb955e394753d
MinecraftDawn <eric4902077@gmail.com>
2019-03-08 23:11:18 +0800

    load a block of schematic file,and generate in new world

--- src/main/cityGenerator/CityGenerator.java
+++ src/main/cityGenerator/CityGenerator.java
@@ -4,13 +4,16 @@ import org.bukkit.WorldCreator;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CityGenerator extends JavaPlugin {
 
+    static Plugin plugin;
+
     @Override
     public void onEnable() {
-
+        plugin = this;
     }
 
     @Override
--- src/main/cityGenerator/testGenerator.java
+++ src/main/cityGenerator/testGenerator.java
@@ -1,15 +1,19 @@
 package main.cityGenerator;
 
+import com.sk89q.worldedit.CuboidClipboard;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.blocks.BaseBlock;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.schematic.SchematicFormat;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
-import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.util.noise.SimplexOctaveGenerator;
 
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
 import java.util.Random;
 
 public class testGenerator extends ChunkGenerator {
@@ -42,11 +46,26 @@ public class testGenerator extends ChunkGenerator {
             }
         }
 
-        return chunk;
-    }
 
-//    @Override
-//    public List<BlockPopulator> getDefaultPopulators(World world) {
-//        return Arrays.asList((BlockPopulator) new testPopulator());
-//    }
+        Location location = new Location(world, chunkX * 16, height, chunkZ * 16);
+
+        WorldEditPlugin worldEditPlugin = (WorldEditPlugin) Bukkit.getPluginManager().getPlugin("WorldEdit");
+
+        File schematic = new File(CityGenerator.plugin.getDataFolder() + File.separator + "Block.schematic");
+
+        EditSession session = worldEditPlugin.getWorldEdit().getEditSessionFactory().getEditSession(new BukkitWorld(world), 5000);
+
+        try {
+            SchematicFormat format = SchematicFormat.getFormat(schematic);
+            CuboidClipboard cc = format.load(schematic);
+            BaseBlock block = cc.getBlock(new Vector(0,0,0));
+            chunk.setBlock(0,height,0,Material.getMaterial(block.getId()));
+        } catch (Exception e) {
+            Bukkit.getServer().getLogger().info("發生錯誤");
+        }
+
+
+        return chunk;
+
+    }
 }
