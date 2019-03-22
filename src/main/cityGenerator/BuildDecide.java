3656c19d5050770e8f85c7e8c872fc33b982068b
MinecraftDawn <eric4902077@gmail.com>
2019-03-22 20:25:41 +0800

    Remove testing code

--- src/main/cityGenerator/BuildDecide.java
+++ src/main/cityGenerator/BuildDecide.java
@@ -44,80 +44,11 @@ public class BuildDecide implements IGenerator {
             }
 
         }
-
-        /*
-        if (chunkX < 0 || chunkZ < 0) return chunk;
-
-        for (int xx = 0; xx < 16; xx++) {
-            for (int zz = 0; zz < 16; zz++) {
-                int x = chunkX * 16 + xx;
-                int z = chunkZ * 16 + zz;
-
-//                if (x < 0 || z < 0) return chunk;
-
-                //int quotientX = Math.abs(x / (streetWidth + buildingWidth));
-                int X = x % (streetWidth + buildingWidth);
-                //int quotientZ = Math.abs(z / (streetWidth + buildingWidth));
-                int Z = z % (streetWidth + buildingWidth);
-
-                //道路
-                if (x < streetWidth) {
-
-                    //建築
-                } else {
-                    int centerX = Math.abs(x) - X + buildingWidth / 2;
-                    int startX = centerX - scheReader.getSize().getBlockX() / 2;
-                    int endX = centerX + scheReader.getSize().getBlockX() / 2;
-                    int centerZ = Math.abs(z) - Z + buildingWidth / 2;
-                    int startZ = centerZ - scheReader.getSize().getBlockZ();
-                    int endZ = centerZ + scheReader.getSize().getBlockZ() / 2;
-
-
-                    if (x > startX && x < endX && z > startZ && z < endZ) {
-                        for (int y = height + 2; y < height + 2 + scheReader.getSize().getBlockY(); y++) {
-                            int BlockID = scheReader.getBlockID(x - startX, y, z - startZ);
-                            byte BlockData = scheReader.getBlockData(x - startX, y, z - startZ);
-                            chunk.setBlock(x%16, y, z%16, BlockID, BlockData);
-                        }
-                    }
-                }
-
-
-            }
-        }
-        */
         return chunk;
     }
 
     public void getBuildType(int x, int y, int z) {
-        /*
-        if (x < 0 || z < 0) return;
 
-        //int quotientX = Math.abs(x / (streetWidth + buildingWidth));
-        int X = Math.abs(x % (streetWidth + buildingWidth));
-        //int quotientZ = Math.abs(z / (streetWidth + buildingWidth));
-        int Z = Math.abs(z % (streetWidth + buildingWidth));
-
-        //道路
-        if (x < streetWidth) {
-
-            //建築
-        } else {
-            int centerX = Math.abs(x) - X + buildingWidth / 2;
-            int startX = centerX - scheReader.getSize().getBlockX() / 2;
-            int endX = centerX + scheReader.getSize().getBlockX() / 2;
-            int centerZ = Math.abs(z) - Z + buildingWidth / 2;
-            int startZ = centerZ - scheReader.getSize().getBlockZ();
-            int endXZ = centerZ + scheReader.getSize().getBlockZ() / 2;
-
-
-            if (x > startX && x < endX && z > startX && z < startZ) {
-
-            }
-
-        }
-        */
     }
-
 
 }
