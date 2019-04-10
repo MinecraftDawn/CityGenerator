20895f9b83c03ed1438106d58a0b99aed82aa2a4
MinecraftDawn <eric4902077@gmail.com>
2019-04-10 20:12:54 +0800

    Remove unusing code

--- src/main/cityGenerator/fileProcess/SchematicManager.java
+++ src/main/cityGenerator/fileProcess/SchematicManager.java
@@ -1,16 +1,12 @@
 package main.cityGenerator.fileProcess;
 
-import main.cityGenerator.CityGenerator;
-
 import java.io.File;
 
 public class SchematicManager {
 
     public SchematicReader scheInfo;
 
-    public SchematicManager(File schematicFile){
-//        File schematicFile = new File(CityGenerator.plugin.getDataFolder()
-//                + File.separator + "Building" + File.separator  +fileName);
+    public SchematicManager(File schematicFile) {
         SchematicFileLoader scheFileLoader = new SchematicFileLoader(schematicFile);
 
         scheInfo = scheFileLoader.getSchematicInfo();
