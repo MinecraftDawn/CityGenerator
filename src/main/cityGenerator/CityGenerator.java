005a2933d098e6ac80b5fa73df8ca26d434419d6
MinecraftDawn <eric4902077@gmail.com>
2019-04-21 18:02:42 +0800

    測試將Air.schematic從jar檔中寫出

--- src/main/cityGenerator/CityGenerator.java
+++ src/main/cityGenerator/CityGenerator.java
@@ -9,6 +9,8 @@ import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.*;
+
 public class CityGenerator extends JavaPlugin {
 
     public static Plugin plugin;
@@ -16,6 +18,31 @@ public class CityGenerator extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
+        /*
+        File f = new File(getClass().getResource("/main/resource/Air.schematic").getFile());
+        File out = new File(getDataFolder().toString()
+                + File.separator + "Test.aaa");
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = new FileInputStream(f);
+            outputStream = new FileOutputStream(out);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+            inputStream.close();
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(f.getPath());
+        System.out.println(f.getName());
+        */
 
         SchematicManager.loadAllSchematic();
     }
